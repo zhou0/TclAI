c61ec56513200cc1a09936d253c3007637f01c04
zhou0 <zhou0@users.noreply.github.com>
2026-05-14 07:03:31 +0800

    Delete test_json_pretty_encoding.tcl

--- test_json_pretty_encoding.tcl
+++
@@ -1,7 +0,0 @@
-lappend auto_path [file join [file dirname [info script]] lib]
-package require llm_ui::logic
-
-set json "{\"content\": \"我是\"}"
-# If we don't decode correctly, it might show incorrectly
-set pretty [::llm_ui::logic::json_pretty $json]
-puts "Pretty JSON: $pretty"
